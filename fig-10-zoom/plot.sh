4ac7b1dde8aeaa5c20625ef1c5a45b02c0260f30
Hendrik Cech <hendrik.cech@gmail.com>
2024-02-17 11:48:39 +0100

    zoom: Respect DATA_PATH

--- fig-10-zoom/plot.sh
+++ fig-10-zoom/plot.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env sh
 
-set -x
+set -xeu
 
 cd "$(dirname -- "$0")"
 
-DATA="../fig-10-zoom-dataset"
-RES="../result"
+DATA="$DATA_PATH/zoom"
+RES="result"
 
-echo "Plot xtime"
+mkdir -p "$RES"
+
+echo "Plotting Fig 10 Zoom"
 python plot_xtime_figure.py \
     "$DATA"/20231011_ter_gpu/01 "$DATA"/20231011_stl_gpu/01 \
     --view-ter 90,145 --view-stl 120,175 \
     --save "$RES/fig-10-zoom.pdf"
+
+echo "Generating Zoom statistics"
+python boxplots.py \
+    --stl  "$DATA"/20231011_stl_gpu/{01,02,03,04,05,06} \
+    --ter  "$DATA"/20231011_ter_gpu/{01,02,03,04,05,06} \
+    --save "$RES/zoom-boxplot.pdf" \
+    --csv  "$RES/zoom_metrics.csv"
